12d595d4484861d574513747da30a6605cd22630
Amin332123 <amin@example.com>
2026-05-26 02:53:16 +0100

    defining the cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://bayanchatfront.onrender.com'],
+    'allowed_origins' => ['http://localhost:3000', 'https://bayanchatfront.onrender.com/'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
